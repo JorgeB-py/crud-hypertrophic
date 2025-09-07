e82adfaefad7844c3c239671251fde1fbebcbd3f
JorgeB <j.bustamantep@uniandes.edu.co>
2025-09-07 15:50:41 -0500

    Fix: build

--- src/app/crud/productos/page.tsx
+++ src/app/crud/productos/page.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useMemo, useState } from 'react'
 import Image from 'next/image'
 import {
   collection, addDoc, updateDoc, deleteDoc, doc,
-  onSnapshot, orderBy, query, serverTimestamp
+  onSnapshot, orderBy, query
 } from 'firebase/firestore'
 import { db } from '@/lib/firebaseClient'
 
